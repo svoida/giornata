defa827d449c66c2dc7f97e5d025c11cdc0297e1
svoida <svoida@b61b6bcf-e9e7-426a-a8b1-a1e724c1f34b>
2009-08-21 21:24:32 +0000

    Added debugging and logging code to main application executable

--- branches/working/core/source/main.m
+++ branches/working/core/source/main.m
@@ -10,5 +10,23 @@
 
 int main(int argc, char *argv[])
 {
+	// Optionally, redirect all program output to a log file (for data collection purposes)
+	if (argc > 1 && strcmp(argv[1], "-nolog") == 0) {
+		NSLog(@"Logging messages to the console (for debugging). No permanent log file will be created.");
+	} else {
+		char *val_buf, path_buf[155];
+		val_buf = getenv("HOME");
+		sprintf(path_buf,"%s/Library/Logs/Giornata.log",val_buf);
+		freopen(path_buf,"a",stderr);
+	}
+	
+#if defined (DEBUG)
+    NSLog(@"Additional debugging features (zombies, freed object check) enabled");
+    
+	NSDebugEnabled = YES;
+    NSZombieEnabled = YES;
+    [NSAutoreleasePool enableFreedObjectCheck:YES];
+#endif /* DEBUG */
+	
     return NSApplicationMain(argc,  (const char **) argv);
 }
